13620b2906c94ef26211e7d85f9b1132164f9b2b
Paul Kononenko <storuky@gmail.com>
2020-02-25 17:16:21 +0300

    Updates

---
+++ precache-manifest.1dd7a125ce38bc808b67822321038fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c90df45f7c098a8cf926791144c7c0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ab5ad3bc9efdf2f48fb7",
+    "url": "/monster-rolodex/static/css/main.8b3a003b.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5ad3bc9efdf2f48fb7",
+    "url": "/monster-rolodex/static/js/main.295ccd80.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
